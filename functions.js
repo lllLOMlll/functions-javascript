10ca13cf70a3e717aec2623082714fe055fd6a36
Louis-Olivier Major <louis.olivier.major@gmail.com>
2025-07-23 07:57:25 -0400

    isEven(n) -> recursive

--- functions.js
+++ functions.js
@@ -12,6 +12,8 @@
 
 // nameAge("Victor", 10);
 
+
+//************************************************************ 
 //MIN
 // function min(number1, number2) {
 //     if (number1 < number2) {
@@ -26,20 +28,48 @@
 //     return number2;
 // }
 
-const min = (num1, num2) => {
-    if (num1 < num2) return num1;
-    return num2
+// const min = (num1, num2) => {
+//     if (num1 < num2) return num1;
+//     return num2
+// }
+
+// console.assert(min(0, 10) === 0);
+// console.assert(min(0, -10) === -10);
+
+// min1 = min(0, 10);
+// min2 = min(0, -10);
+// console.log(min1);
+// console.log(min2);
+
+
+//***************************
+//isEven(n)
+// function isEven(n) {
+//     if (n < 0) return isEven(-n)
+    
+//     if (n === 0) return true;
+//     if (n === 1) return false;
+
+//     return isEven(n-2);
+// }
+
+// console.assert(isEven(50) === true);
+// console.assert(isEven(75) === false);
+// console.assert(isEven(-1) === false);
+
+
+//************************************************ 
+//Exercice 3 – Compter des caractères (countChar)
+function countChar(str, char) {
+    let strLenght = str.length;
+    let countChar = 0;
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] == char) {
+            countChar += 1;
+        }
+    }
 }
 
-console.assert(min(0, 10) === 0);
-console.assert(min(0, -10) === -10);
-
-min1 = min(0, 10);
-min2 = min(0, -10);
-console.log(min1);
-console.log(min2);
-
-
-
-
+console.assert(countChar("Bonjour", "o") === 2);
+console.assert(countBs("BBC") === 2);
 
